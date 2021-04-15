732704883ddb64bab4402031322a458446b3e835
JBouchier <S3909589@student.rmit.edu.au>
2021-04-15 14:57:53 +1000

    Refactoring Again

--- scripts.js
+++
@@ -1,9 +0,0 @@
-const navLinks = document.getElementsByTagName("a");
-
-function onBtnClick(e) {
-    for (btn of navLinks) btn.classList.remove("selected");
-    if (e.id !="logo") e.classList.add("selected");
-    else {
-        document.location.hash = "#home";
-    }
-}
